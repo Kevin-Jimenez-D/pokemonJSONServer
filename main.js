48b5edaf18e647aebd24bd6b782caa3f2ef1853a
“Kevin-Jimenez-D” <kevinjimenezd97@gmail.com>
2023-09-29 13:11:13 -0500

    feat: :sparkles: Logré que me envíe los pokemones que existen desde el mockapi creado hasta JSONServer
    En este caso me trae todos los datos de los pokemones desde la mockapi, estoy considerando que se traigan desde pokeapi, porque ahí vienen inmersas las imágenes que  usaré para que se vean la pagina

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hola")
+//Traer información desde la MockApi hasta "http://localhost:3000/pokemonesPokeApi", solo hacerse una vez, hacerse con el boton de pokeapiMockapi
+const traerMockApi = async()=>{
+    let mockapi = await (await fetch("https://6512485eb8c6ce52b3957baa.mockapi.io/pokemon")).json();
+    //console.log(mockapi);
+    let config=
+    {
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(mockapi)
+    }
+
+    let res = await (await fetch("http://localhost:3000/pokemonesPokeApi", config)).json();
+    console.log(res);
+}
+
+traerMockApi();
